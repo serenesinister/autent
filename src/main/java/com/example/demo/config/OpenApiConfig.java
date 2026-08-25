03274c453f38e7a341d10eae06f725b20803cdc9
serenesinister <wemersonsilvasoares9@gmail.com>
2026-08-25 11:03:00 -0300

    configura informacoes da API no Swagger

---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User API",
+                version = "1.0",
+                description = "API para gerenciamento de usuarios com autenticacao JWT"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
